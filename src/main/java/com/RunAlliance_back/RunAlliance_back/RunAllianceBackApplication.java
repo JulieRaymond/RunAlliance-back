e7c0b1676cd282f9ceaeb3171a94f3870e200e5f
Julie Raymond <julie.raymond@pichet.com>
2024-08-26 11:11:51 +0200

    Added : pom.xml + application properties init

---
+++ src/main/java/com/RunAlliance_back/RunAlliance_back/RunAllianceBackApplication.java
@@ -0,0 +1,13 @@
+package com.RunAlliance_back.RunAlliance_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunAllianceBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunAllianceBackApplication.class, args);
+	}
+
+}
